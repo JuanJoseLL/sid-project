146bfc767cf66fdb6784b0966526054e7794d3bd
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2024-05-28 12:05:42 -0500

    feat: Add conn to Oracle DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,19 +10,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentModule } from './comments/comment.module';
 import { PeopleModule } from './people/people.module';
 
+oracledb.initOracleClient({ libDir: 'C:\\instantclient_21_13' });
+
 @Module({
-  imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'oracle',
-    //   host: '200.3.193.24',  // O usa '172.16.0.103' si estás dentro de la red interna
-    //   port: 1522,
-    //   username: 'P09779_1_2',
-    //   password: 'MFQHqFMxVp',
-    //   sid: 'ESTUD',          // Utiliza 'sid' en lugar de 'serviceName' para conexiones Oracle
-    //   synchronize: true,
-    //   logging: true,  // Habilitar el logging
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    // }),
+  imports: [   
+    TypeOrmModule.forRoot({
+      type: 'oracle',
+      connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=200.3.193.24)(PORT=1522))(CONNECT_DATA=(SERVICE_NAME=ESTUD)))",
+      username: 'P09779_1_2',
+      password: 'MFQHqFMxVp',
+      synchronize: true,
+      logging: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
     CacheModule.register({
         store: redisStore,
         ttl: 30 * 1000, // Los elementos en caché se borran después de 30 segundos
